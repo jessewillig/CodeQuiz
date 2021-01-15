f4004b4f4c31e106cddf9a41e27c7e30e37cf764
Jesse Willig <jessewillig@outlook.com>
2021-01-14 17:38:17 -0800

    started js

---
+++ script.js
@@ -0,0 +1,13 @@
+var home = document.querySelector(".home");
+var scores = document.querySelector(".scores");
+var play = document.querySelector(".play");
+var start = document.querySelector(".start")
+var game = document.querySelector(".game")
+
+home.addEventListener("click", function() {
+    if (condition) {
+        
+    } else {
+        
+    }
+}
